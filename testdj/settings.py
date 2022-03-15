f0ee736ebd1e3a7af923fe1f4d89d6656527dfac
Pooja <poojaboodi315@gmail.com>
2022-03-15 20:41:47 +0530

    c

--- testdj/settings.py
+++ testdj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j=y5twv@b+cfz8544-!xxwioq2#oq^d6acr1^w95$j@l8sq$-1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["djdeployy.heroku.com","localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
